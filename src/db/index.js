c2b21ca95ff17dac3dfa2cd1d0d5818445ac01c4
AnandBhaskarx <aanandbhaskarx@gmail.com>
2025-07-08 20:32:53 +0530

    aggregate piplining , jwt ,bcrypt

--- src/db/index.js
+++ src/db/index.js
@@ -11,6 +11,6 @@ const connectDB = async ()=>{
         console.log("mongoDB connection error",error);
         process.exit(1);
     }
-}
+} 
 
 export default connectDB
---
+++ src/models/user.models.js
@@ -0,0 +1,91 @@
+import mongoose,{Schema} from 'mongoose'
+import  jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+    {
+
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+        },
+        fullname:{
+            type:String,
+            required:true, 
+            lowercase:true,
+            trim:true,
+        },
+        avatar:{
+            type:String, //cloudinary url
+            required:true,
+        },
+        coverImage:{
+            type:String,
+        },
+        wathHistory: [
+            {
+                type:Schema.Types.ObjectId,
+                ref:"Video"
+            }
+        ],
+        password:{
+            type:String,
+            required:[true,"password is required"]
+        },
+        refreshToken:{
+            type:String
+        }
+    },{
+        timestamps:true
+    }
+);
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password"))return next(); //it can run every time while user changing any field
+    //thats why we applied this if condition so if password is not modified it moves to next ,, other wise update and encryptt password
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+   return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methods.generateRefreshToken=function(){
+     return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    })
+}
+
+export const User = mongoose.models("User",userSchema)
